fe79026d528f9defed475d1464b76695ce987ba5
Abid Hasan <abid.northsouth@gmail.com>
2019-10-29 17:48:08 +0600

    Implemented data fetcher interface

--- src/main/kotlin/com/abid/springraphql/service/datafetcher/AuthorFromBookDataFetcher.kt
+++ src/main/kotlin/com/abid/springraphql/service/datafetcher/AuthorFromBookDataFetcher.kt
@@ -2,7 +2,13 @@ package com.abid.springraphql.service.datafetcher
 
 import com.abid.springraphql.model.Author
 import graphql.schema.DataFetcher
+import graphql.schema.DataFetchingEnvironment
+import org.springframework.stereotype.Component
 
+@Component
 class AuthorFromBookDataFetcher : DataFetcher<List<Author>> {
+    override fun get(environment: DataFetchingEnvironment?): List<Author> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
